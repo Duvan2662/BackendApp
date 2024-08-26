e0f99658fd457a915d447454ee65cb380a377f95
Duvan <duvanriano2626@gmail.com>
2024-08-26 12:29:09 -0500

    Crear el modelo y el esquema de la base de datos Importante

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+    // _id:string; //Mongo lo hace por mi
+
+    @Prop({unique:true, required:true})
+    name:string;
+
+    @Prop({required:true})
+    email:string;
+
+    @Prop({minlength:6, required:true})
+    password:string;
+
+    @Prop({default:true})
+    isActive:string;
+
+    @Prop({type:[String], default:['user']}) //[['user','Admin', ....]
+    roles:string[];
+}
+
+
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
