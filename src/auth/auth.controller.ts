944acfc665afe061b6f4c2a64567cac56fe49dee
Duvan <duvanriano2626@gmail.com>
2024-08-26 14:36:01 -0500

    endpoint login - Verificacion de correo y contraseña se esta usando una libreria bcryptjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login.dto';
 
 
 @Controller('auth')
@@ -12,6 +13,11 @@ export class AuthController {
   create(@Body() createUserDto: CreateUserDto) {
     // console.log(createUserDto);
     return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto:LoginDto){
+    return this.authService.login(loginDto);
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import * as bcryptjs from "bcryptjs";
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -46,6 +47,33 @@ export class AuthService {
     }
 
     
+  }
+
+  async login (loginDto:LoginDto) {
+    // console.log({loginDto});
+    const {email, password} = loginDto
+
+    const user = await this.userModel.findOne({email:email});
+    if (!user) {
+      throw new UnauthorizedException('Not valid credentials - email')
+    }
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials - Password')
+    }
+
+    const {password:_, ...rest} = user.toJSON()
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+
+    }
+
+    
+    /**  
+      * User {_id, nam, email, roles}
+      * Token -> ASDASDA.ASDASDA.ASDASDA
+    */
   }
 
   findAll() {
